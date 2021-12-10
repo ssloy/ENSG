226da8b9d7a1da67387a7936b87dcb79895301fc
Dmitry V. Sokolov <ha@haqr.eu>
2021-12-10 08:52:51 +0100

    fault opposite attribute

--- precompute_halfedge_attributes.py
+++ precompute_halfedge_attributes.py
@@ -1,34 +1,47 @@
 from mesh import Mesh
 import numpy as np
 
-def is_edge_present(a, b, h):
+def vec3_equal_vec3(a,b):
     threshold = 1e-6
+    return np.dot(a-b, a-b)<threshold
+
+def is_edge_present(a, b, h):
     for c in range(h.ncorners):
         vi = h.V[h.org(c)]
         vj = h.V[h.dst(c)]
-        if (np.dot(vi-a, vi-a)<threshold and np.dot(vj-b, vj-b)<threshold) or (np.dot(vj-a, vj-a)<threshold and np.dot(vi-b, vi-b)<threshold):
-            return True;
+        if (vec3_equal_vec3(vi, a) and vec3_equal_vec3(vj, b)) or (vec3_equal_vec3(vi, b) and vec3_equal_vec3(vj, a)):
+            return True
     return False
 
 models = [("chevron", 4), ("ifp1", 2), ("ifp2", 2), ("shell", 6)]
 for model,nhor in models:
-	m = Mesh(model+"/slice.obj")
-	horizon_meshes = [ Mesh(model+"/horizon"+str(i)+".obj") for i in range(1,nhor+1) ]
-	faults_mesh = Mesh(model+"/faults.obj")
- 
-	horizon_id = [-1]*m.ncorners
-	is_fault   = [ False ]*m.ncorners
- 
-	for c in range(m.ncorners): # for all half-edges
-	    vi = m.V[m.org(c)]
-	    vj = m.V[m.dst(c)]
-	    for (hid, mh) in enumerate(horizon_meshes):
-        	if is_edge_present(vi, vj, mh):
-	            horizon_id[c] = hid
-	    if is_edge_present(vi, vj, faults_mesh):
-	        is_fault[c] = True
+    m = Mesh(model+"/slice.obj")
+    horizon_meshes = [ Mesh(model+"/horizon"+str(i)+".obj") for i in range(nhor) ]
+    faults_mesh = Mesh(model+"/faults.obj")
 
-	out = open(model+"/attributes.py", 'w')
-	print("horizon_id = " + str(horizon_id), file = out)
-	print("is_fault = " + str(is_fault), file = out)
-	out.close()
+    horizon_id = [-1]*m.ncorners
+    is_fault   = [ False ]*m.ncorners
+    fault_opposite = [-1]*m.ncorners
+
+    for c in range(m.ncorners): # for all half-edges
+        vi = m.V[m.org(c)]
+        vj = m.V[m.dst(c)]
+        for (hid, mh) in enumerate(horizon_meshes):
+            if is_edge_present(vi, vj, mh):
+                horizon_id[c] = hid
+        if is_edge_present(vi, vj, faults_mesh):
+            is_fault[c] = True
+
+    fault_corners = [ c for c in range(m.ncorners) if is_fault[c] ]
+    for c1 in fault_corners:
+        for c2 in fault_corners:
+            if c1==c2: continue
+            if vec3_equal_vec3(m.V[m.org(c1)], m.V[m.dst(c2)]) and vec3_equal_vec3(m.V[m.org(c2)], m.V[m.dst(c1)]):
+                fault_opposite[c1] = c2
+                fault_opposite[c2] = c1
+
+    out = open(model+"/attributes.py", 'w')
+    print("horizon_id = " + str(horizon_id), file = out)
+    print("is_fault = " + str(is_fault), file = out)
+    print("fault_opposite = " + str(fault_opposite), file = out)
+    out.close()
--- test_attributes.py
+++ test_attributes.py
@@ -6,12 +6,12 @@ m = Mesh(model+"/slice.obj")
 attr = importlib.import_module(model + ".attributes")
 
 for c in range(m.ncorners): # lift all vertices of all horizons
-	if attr.horizon_id[c]>=0:
-		height = (1+attr.horizon_id[c])/37.76 # arbitrarily chosen coeff to get a visually nice result
-		m.V[m.org(c)][2] = m.V[m.dst(c)][2] = height
+    if attr.horizon_id[c]>=0:
+        height = (1+attr.horizon_id[c])/37.76 # arbitrarily chosen coeff to get a visually nice result
+        m.V[m.org(c)][2] = m.V[m.dst(c)][2] = height
 
 for c in range(m.ncorners): # lower vertices in faults
-	if attr.is_fault[c]:
-		m.V[m.org(c)][2] -= 0.00431 # arbitrary scaling coefficent
-		m.V[m.dst(c)][2] -= 0.00431 # to make the result look nice
+    if attr.is_fault[c]:
+        m.V[m.org(c)][2] -= 0.00431 # arbitrary scaling coefficent
+        m.V[m.dst(c)][2] -= 0.00431 # to make the result look nice
 print(m)
